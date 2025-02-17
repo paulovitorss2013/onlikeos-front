5932a176bb1543ac61239b97f8be37a238b5761b
paulovitorss2013 <paulovitor_santos@hotmail.com.br>
2025-02-17 17:13:16 -0300

    Ajustes responsivos para técnico ok

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -3,6 +3,7 @@ import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { Tecnico } from '../../../models/tecnico';
 import { TecnicoService } from '../../../services/tecnico.service';
+import { MatPaginatorIntl } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-tecnico-list',
@@ -12,22 +13,23 @@ import { TecnicoService } from '../../../services/tecnico.service';
 export class TecnicoListComponent implements OnInit {
 
   ELEMENT_DATA: Tecnico[] = []
-
   displayedColumns: string[] = ['nome', 'cpf', 'acoes'];
   dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(
-    private service:TecnicoService
-
-  ) {}
+    private service: TecnicoService,
+    private paginatorIntl: MatPaginatorIntl
+  ) {
+    this.paginatorIntl.itemsPerPageLabel = ''; 
+    this.paginatorIntl.getRangeLabel = () => '';
+  }
 
   ngOnInit(): void {
     this.findAll();
   }
 
-  // MÉTODO PARA BUSCAR TODOS OS TÉCNICOS
   findAll() {
     this.service.findAll().subscribe(resposta => {
       this.ELEMENT_DATA = resposta;
@@ -36,10 +38,8 @@ export class TecnicoListComponent implements OnInit {
     });
   }
 
-  // APLICA FILTRO PARA A BUSCA DE TODOS OS TÉCNICOS
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
-
 }
