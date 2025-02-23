912a9997b28b9ddfc61778c4c7b1e37c0c346be1
paulovitorss2013 <paulovitor_santos@hotmail.com.br>
2025-02-23 14:37:57 -0300

    Valida Campos chamado-create ok

--- src/app/components/chamado/chamado-create/chamado-create.component.ts
+++ src/app/components/chamado/chamado-create/chamado-create.component.ts
@@ -1,34 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-import { Chamado } from '../../../models/chamado';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-chamado-create',
   templateUrl: './chamado-create.component.html',
-  styleUrl: './chamado-create.component.css'
+  styleUrls: ['./chamado-create.component.css'] // CORREÇÃO DE styleUrl PARA styleUrls
 })
 export class ChamadoCreateComponent implements OnInit {
 
   // GRUPO DE FORMULÁRIOS REATIVOS
   form: FormGroup = new FormGroup({
-
+    titulo: new FormControl('', [Validators.required, Validators.minLength(10)]),
+    prioridade: new FormControl('', [Validators.required]),
+    status: new FormControl('', [Validators.required]),
+    descricao: new FormControl('', [Validators.required, Validators.minLength(15)]),
+    tecnico: new FormControl('', [Validators.required]),
+    cliente: new FormControl('', [Validators.required])
   });
 
   // CONSTRUTOR
-  constructor( ) {}
+  constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // MONITORAR ALTERAÇÕES NO FORMULÁRIO
+    this.form.statusChanges.subscribe(status => {
+      console.log("Status do formulário:", status);
+    });
+  }
 
- // MÉTODO PARA CRIAR UM CLIENTE
-create(): void {
-
-}
+  // MÉTODO PARA CRIAR UM CHAMADO
+  create(): void {
+    if (this.form.valid) {
+      console.log("Chamado criado:", this.form.value);
+    } else {
+      console.log("Erro: Formulário inválido!");
+    }
+  }
 
   // MÉTODO PARA CONFIRMAR O CANCELAMENTO DAS AÇÕES
   confirmarCancelamento(): void {
     if (window.confirm('Deseja mesmo cancelar?')) {
-
+      console.log("Ação cancelada!");
     }
   }
 
