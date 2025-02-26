eb53d514bf0c59a852bd1564088fb6947eb99e74
paulovitorss2013 <paulovitor_santos@hotmail.com.br>
2025-02-26 14:27:47 -0300

    Funções botões tela home ok

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+    
+// CONSTRUTOR
+  constructor(
+    private router: Router,
+  ) {}
+
+// MÉTODO LISTAR TODOS OS TÉCNICOS
+listarTecnicos(): void {
+  this.router.navigate(['tecnicos']);
+ } 
+
+// MÉTODO NAVEGAR PARA LISTAR TODOS OS CHAMADOS
+listarClientes(): void {
+  this.router.navigate(['clientes']);
+ }
+
+// MÉTODO NAVEGAR PARA LISTAR TODOS OS CHAMADOS
+listarChamados(): void {
+  this.router.navigate(['chamados']);
+ }
 
 }
