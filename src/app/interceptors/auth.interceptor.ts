642be0cc2760d3c69e4da268b09d7aced695ff99
paulovitorss2013 <63860432+paulovitorss2013@users.noreply.github.com>
2025-03-04 21:03:32 -0300

    Ajuste CSS tecnico-create

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -13,24 +13,34 @@ export class AuthInterceptor implements HttpInterceptor {
       const token = localStorage.getItem('token');
 
       if (token) {
-        request = request.clone({
+        const cloneReq = request.clone({
           headers: request.headers.set('Authorization', `Bearer ${token}`)
         });
+        return next.handle(cloneReq).pipe(
+          catchError((error) => this.handleError(error))
+        );
       } 
 
-      return next.handle(request).pipe(catchError((error) => this.handleError(error))); // Aplicando o catchError uma única vez
+      return next.handle(request).pipe(
+        catchError((error) => this.handleError(error))
+      );
   }
 
   private handleError(error: HttpErrorResponse): Observable<never> {
     if (error.status === 403) {
       const errorMessage = error.error?.message || 'Você não tem permissão para essa ação!';
-      if (!this.router.url.includes('/login')) {
+      const isLoginPage = this.router.url.includes('/login');
+  
+      if (!isLoginPage) {
         this.toastr.warning(errorMessage, 'Acesso Negado');
       }
+  
+      return throwError(() => error);
     }
   
     if (error.status === 401) {
       localStorage.removeItem('token');
+      this.toastr.warning('Você foi desconectado. Por favor, faça login novamente.', 'Acesso Negado');
       this.router.navigate(['/login']);
     }
   
